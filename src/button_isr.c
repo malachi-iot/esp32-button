4806f3a6c2f251a2ac6bd7d0cf0d72aece12a826
Malachi Burke <malachi.burke@gmail.com>
2022-04-27 17:27:30 -0700

    Splitting DOWN logic out into timer

--- src/button_isr.c
+++ src/button_isr.c
@@ -13,12 +13,14 @@ extern int pin_count;
 extern debounce_t * debounce;
 extern QueueHandle_t queue;
 
-static gpio_isr_handle_t isr_handle;
+static gpio_isr_handle_t gpio_isr_handle;
 static timer_isr_handle_t timer_isr_handle;
 
 static unsigned down_events = 0;
 
 static const char* TAG = "esp32 Button ISR";
+
+static int timer_group = TIMER_GROUP_0;
 
 static void update_button_history(debounce_t *d, 
     unsigned time_passage,
@@ -74,7 +76,7 @@ static unsigned threshold_ms = 50;
 
 // FIX: Not valid for multiple buttons and not debouncing anything at all
 // Just getting things compiling and running for now
-static void button_isr(void* context) {
+static void gpio_isr(void* context) {
     // Guidance from:
     // https://esp32.com/viewtopic.php?t=345 
     // 
@@ -87,40 +89,69 @@ static void button_isr(void* context) {
     // https://www.esp32.com/viewtopic.php?t=20123 indicates we can call this here
     uint32_t millis = esp_timer_get_time() / 1000;
 
-    ets_printf("1 Intr\n");
+    ets_printf("1 gpio Intr\n");
 
     for (int idx=0; idx<pin_count; idx++) {
-        uint32_t gpio_num = 0;
-        if(gpio_intr_status & BIT(gpio_num)) { //gpio0-gpio31
-            debounce_t* const d =  &debounce[idx];
-            int level = gpio_get_level(gpio_num);
+        debounce_t* const d =  &debounce[idx];
 
-            ets_printf("1 Intr GPIO%d, val: %d\n",gpio_num, level);
-            unsigned time_passage;
+        if(gpio_intr_status & BIT(d->pin)) { //gpio0-gpio31
+            int level = gpio_get_level(d->pin);
 
-            if(d->down_time != 0)
-            {
-                time_passage = (millis - d->down_time) / threshold_ms;
-            }
-            else
-                time_passage = 1;
+            ets_printf("1 Intr GPIO%d, val: %d\n",d->pin, level);
 
-            // backfill all the passed time with previous level setting
-            if(time_passage > 1) {
-                // We may get way past our window.  Make sure we don't
-                // flip out in that case
-                if(time_passage < 32)
-                    update_button_history(d, time_passage, !level);
+            // We're only looking to debounce the up part here
+            // The downpress part we're sniffing in the timer
+            if(level == 1) {
+                unsigned time_passage;
+
+                if(d->down_time != 0)
+                {
+                    time_passage = (millis - d->down_time) / threshold_ms;
+
+                    // We toss bounciness in a sub threshold range
+                    if(time_passage == 0)
+                        continue;
+                }
                 else
-                    // FIX: Not correct here
-                    update_button_history(d, 32, !level);
+                    time_passage = 1;
+
+                // backfill all the passed time with previous level setting
+                if(time_passage > 1) {
+                    // We may get way past our window.  Make sure we don't
+                    // flip out in that case
+                    if(time_passage < 32)
+                        update_button_history(d, time_passage, !level);
+                    else
+                        // FIX: Not correct here
+                        update_button_history(d, 32, !level);
+                }
+
+                update_button(d, level);
+            }
+            else {
+                // Kick off timer to initiate:
+                // - DOWN event once it's been down long enough
+                // - HELD event once it's been down that much further
             }
 
-            update_button(d, level);
-
-            d->down_time = millis;
-            ++down_events;
+            //d->down_time = millis;
+            //++down_events;
             //send_event(d, BUTTON_DOWN);
+            timer_start(timer_group,0);
+        }
+
+        if (button_up(d)) {
+            d->down_time = 0;
+            send_event(d, BUTTON_UP);
+            /* HELD to be assisted by timer ISR
+        } else if (d->down_time && millis() >= debounce[idx].next_long_time) {
+            debounce[idx].next_long_time = debounce[idx].next_long_time + CONFIG_ESP32_BUTTON_LONG_PRESS_REPEAT_MS;
+            send_event(d, BUTTON_HELD); */
+            /* DOWN also to be done by timer ISR
+        } else if (button_down(d) && d->down_time == 0) {
+            d->down_time = millis;
+            d->next_long_time = d->down_time + CONFIG_ESP32_BUTTON_LONG_PRESS_DURATION_MS;
+            send_event(d, BUTTON_DOWN); */
         }
     }
 }
@@ -145,7 +176,7 @@ QueueHandle_t esp32_button_init_internal(
     // considering the wiggle of the bounce
     
     ESP_ERROR_CHECK(
-        gpio_isr_register(button_isr, NULL, ESP_INTR_FLAG_LEVEL1, &isr_handle));
+        gpio_isr_register(gpio_isr, NULL, ESP_INTR_FLAG_LEVEL1, &gpio_isr_handle));
 
     // Scan the pin map to determine number of pins
     pin_count = 0;
@@ -175,16 +206,34 @@ QueueHandle_t esp32_button_init_internal(
     return queue;
 }
 
+static uint32_t ms_test = 0;
+
 // Guidance from
 // https://www.esp32.com/viewtopic.php?t=12931 
 static void IRAM_ATTR timer_group0_isr (void *param){
     TIMERG0.int_clr_timers.t0 = 1; //clear interrupt bit
+    TIMERG0.hw_timer[0].config.alarm_en = 1;    // re-enable alarm
 
     uint32_t millis = esp_timer_get_time() / 1000;
+
+    if(ms_test == 0) {
+        ets_printf("1 timer Intr millis=%u\n", millis);
+    }
+
+    ms_test = millis;
 
     if(down_events > 0) {
         for (int idx=0; idx<pin_count; idx++) {
             debounce_t* const d =  &debounce[idx];
+            // TODO: Doesn't handle 0=up yet
+            int level = 0;
+            update_button(d, level);
+
+            if(button_down(d) && d->down_time == 0) {
+                d->down_time = millis;
+                d->next_long_time = d->down_time + CONFIG_ESP32_BUTTON_LONG_PRESS_DURATION_MS;
+                send_event(d, BUTTON_DOWN);
+            }
         }
     }
 }
@@ -193,7 +242,6 @@ QueueHandle_t button_init_isr(unsigned long long pin_select) {
     QueueHandle_t queue = esp32_button_init_internal(pin_select, GPIO_FLOATING);
     
     timer_config_t timer;
-    int timer_group = TIMER_GROUP_0;
     
     // Set prescaler for 10 KHz clock.  We'd go slower if we could, but:
     // "The divider’s range is from from 2 to 65536."
@@ -210,11 +258,18 @@ QueueHandle_t button_init_isr(unsigned long long pin_select) {
     timer_isr_register(timer_group, 0, timer_group0_isr, NULL, 
         ESP_INTR_FLAG_LEVEL1 | ESP_INTR_FLAG_IRAM,
         &timer_isr_handle);
+    
+    // Brings us to an alarm every 10ms (100 counting / 10 Khz = 0.01s = 10ms)
+    timer_set_alarm_value(timer_group, 0, 100);
+
+    timer_enable_intr(timer_group,0);
 
     return queue;
 }
 
 void button_deinit_isr() {
-    esp_intr_free(isr_handle);
+    timer_disable_intr(timer_group,0);
+    esp_intr_free(timer_isr_handle);
+    esp_intr_free(gpio_isr_handle);
     button_deinit();
 }
